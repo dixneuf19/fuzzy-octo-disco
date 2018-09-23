71e32514fd25e192bd52375290fc67a5cb7b7a84
Julen Dixneuf <julen.dixneuf@student.ecp.fr>
2018-09-23 21:38:15 +0200

    add a cli and allows use for bot

--- process_pic_CLI.py
+++ process_pic_CLI.py
@@ -1,7 +1,55 @@
 #-*- coding: utf-8 -*-
 from pic import Picture
 from path import Path
+import argparse
 import copy
+
+parser = argparse.ArgumentParser(
+    description="Process the picture or the directory, given the json config file")
+
+
+parser.add_argument("path", help="Path for the picture or the directory")
+parser.add_argument(
+    "-n",
+    "--nb_faces",
+    type=int,
+    help="0 | 1 | n to don't search for any face | search and expect exactly one face | search for multiple face"
+)
+
+parser.add_argument(
+    "--margin",
+    "-m",
+    type=float,
+    help="Specify the margin around the face if the face_crop is activate")
+
+parser.add_argument("--json", "-j", help="Path to a config file")
+
+parser.add_argument(
+    "--rotate", default=True, help="Try to rotate the picture to find faces")
+
+parser.add_argument(
+    "--skip_compression", action='store_true', help="skip compression")
+
+parser.add_argument(
+    "--resolution",
+    "-r",
+    help="ratio and resolution for the resize of the picture, for example 240x300")
+
+parser.add_argument(
+    "--face_crop",
+    action="store_false",
+    help="crop around the faces found on picture, activated by default")
+
+parser.add_argument("--out_path", help="path for the output file(s)")
+
+parser.add_argument("--out_tag", help="tag for the output file(s)")
+
+parser.add_argument(
+    "--max_size", help="max size in bytes for the output file(s)")
+
+parser.add_argument(
+    "--ext",
+    help="choose the extension of the output file(s) : .jpg, .gif, .png")
 
 
 def load_json_config(path):
@@ -57,77 +105,6 @@ def save(im, out_path, out_tag, max_size, file_path, extension):
     else:
         print("The file is succesfully saved at", out_path.abspath())
 
-
-def process_pic_for_bot(file_path,
-                resolution,
-                out_tag,
-                out_path="",
-                face_crop=False,
-                nb_faces=1,
-                margin=0.4,
-                rotate=True,
-                max_size=10000,
-                extension=".jpg",
-                skip_compression=False):
-
-    if not (file_path.ext.lower() in (".jpg", ".jpeg", ".png", ".gif")):
-        print("The type of", file_path, "isn't supported.")
-        return
-    print("------------------------------------------------------------------")
-    print("Working on ", file_path)
-
-    # create the Picture instance
-    im = Picture(file_path)
-    # try to open it
-    try:
-        im.open()
-    except OSError:
-        return
-
-    # find faces, exit if it doesn't respect the nb_faces argument
-    if nb_faces >= 1:
-        nb_faces_found = find_faces(im, nb_faces, rotate)
-
-        if nb_faces_found == 0:
-            return
-        # if everything went alright
-        print("Found", im.nb_face, "face(s).")
-
-    # crop around the face
-
-    faces_result = []
-
-    if im.nb_face > 0:
-
-        for i in range(im.nb_face):
-
-            print("Working on face", str(i + 1))
-
-            face = copy.deepcopy(im)
-            end_flag = False
-            while not end_flag:
-                face.face_crop(resolution, margin, whichface=i)
-                if face.cut_error:
-                    margin -= 0.05
-                    if margin < 0:
-                        return
-
-                    print(
-                        "The crop can't fit, now trying with a smaller margin of",
-                        margin)
-                    face.cut_error = 0
-                else:
-                    end_flag = True
-            face.save(out_path + out_tag + str(i) + "_" +
-                          file_path.namebase + extension)        
-            faces_result.append(out_path + out_tag + str(i) + "_" +
-                          file_path.namebase + extension)
-    
-    return faces_result
-
-    
-
-    
 
 def process_pic(file_path,
                 resolution,
@@ -202,18 +179,28 @@ def process_pic(file_path,
         save(im, out_path, out_tag, max_size, file_path, extension)
 
 
-def run_bot(path):
+if __name__ == '__main__':
 
+    args = parser.parse_args()
 
-    config = load_json_config('config.json')
-    
+    if args.json:
+        config = load_json_config(args.json)
+        x, y = config["resolution"].split("x")
+        config["resolution"] = (int(x), int(y))
+    else:
+        config = {}
         
+    # Shell options takes priority over json config
+    args = vars(args)
+    for key, value in args.items():
+        if not value is None:
+            config[key] = value
 
     c = config  #  shorter name
-    path = Path(path)
+    path = Path(c["path"])
     if path.isfile():
         # Assume that the destination is a file
-        return process_pic_for_bot(path, None, c["out_tag"], c["out_path"],
+        process_pic(path, c["resolution"], c["out_tag"], c["out_path"],
                     c["face_crop"], c["nb_faces"], c["margin"], c["rotate"],
                     c["max_size"], c["extension"])
     elif path.isdir():
@@ -221,7 +208,7 @@ def run_bot(path):
         for file in path.files():
             if file.isfile():
                 if file.ext in (".jpg", ".jepg", ".png", ".gif"):
-                    return process_pic(file, c["resolution"], c["out_tag"],
+                    process_pic(file, c["resolution"], c["out_tag"],
                                 c["out_path"], c["face_crop"], c["nb_faces"],
                                 c["margin"], c["rotate"], c["max_size"],
                                 c["extension"], c["skip_compression"])
